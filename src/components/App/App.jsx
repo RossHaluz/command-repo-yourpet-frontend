39df302a4e26a92923cb79da6590a32bc0851889
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2023-05-17 23:56:23 +0300

    prepared

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+ const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      Good luck:)
+    </div>
+  );
+};
+
+export default App
