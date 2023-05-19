c621aff0ed7f5c5a0b3a0e2b6966311973c6757f
RossHaluz <106879174+RossHaluz@users.noreply.github.com>
2023-05-19 13:28:09 +0000

    Deploying to gh-pages from @ RossHaluz/command-repo-yourpet-frontend@69feaa046861f74441067a505a676c1acbf22412 🚀

--- static/js/17.9e02e2e8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[17],{7569:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184),u=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("h2",{children:"Find your pet"}),(0,r.jsx)("input",{type:"text",placeholder:"Seach..."})]})},c=function(){return(0,r.jsx)(u,{})}}}]);
-//# sourceMappingURL=17.9e02e2e8.chunk.js.map
