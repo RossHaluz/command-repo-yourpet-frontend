8ec7e676cb48de0d9f48a2489fbde11dba707894
Николай Черниций <kolac300@gmail.com>
2023-05-21 11:43:20 +0300

    navUpdates

--- src/pages/NoticesPage.js
+++ src/pages/NoticesPage.js
@@ -1,7 +1,16 @@
+
+import NoticesCategoriesNav from 'components/NoticesCategoriesNav/NoticesCategoriesNav';
 import NoticesSearch from 'components/NoticesSearch';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const NoticesPage = () => {
-  return <NoticesSearch />;
-};
+  return (
+    <>
+      <NoticesSearch />
+      <NoticesCategoriesNav/>
+    </>
+  )
+}
 
 export default NoticesPage;
