c6427f5100281943036e7482b2aa1340be743d35
Николай Черниций <kolac300@gmail.com>
2023-05-21 13:33:27 +0300

    AddPetBtnUpdate

--- src/pages/NoticesPage.js
+++ src/pages/NoticesPage.js
@@ -1,14 +1,14 @@
 
+import NoticesAddPetBtn from 'components/NoticesAddPetBtn/NoticesAddPetBtn';
 import NoticesCategoriesNav from 'components/NoticesCategoriesNav/NoticesCategoriesNav';
 import NoticesSearch from 'components/NoticesSearch';
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
 
 const NoticesPage = () => {
   return (
     <>
       <NoticesSearch />
       <NoticesCategoriesNav/>
+      <NoticesAddPetBtn/>
     </>
   )
 }
