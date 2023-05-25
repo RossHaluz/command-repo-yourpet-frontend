141bc7152e8d9e5bf2632f7aebf8f4b163d648dc
IgorSokolyanskyy <1988isokol@gmail.com>
2023-05-25 15:15:19 +0300

    add edit photo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,21 +2,21 @@ import SharedLayout from 'components/SharedLayout';
 import { lazy, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-import Theme from '../../theme/Theme';
 import { getCurrentUser } from 'redux/auth/operetions';
 import { useDispatch } from 'react-redux';
 import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
 import { PublicRoute } from 'components/PublicRoute/PublicRoute';
 import MobileMenuMain from 'components/MobileMenu/MobileMenu';
+import Theme from 'theme/Theme';
 
-const LoginPage = lazy(() => import('../../pages/LoginPage'));
-const RegisterPage = lazy(() => import('../../pages/RegisterPage'));
-const MainPage = lazy(() => import('../../pages/MainPage'));
-const UserPage = lazy(() => import('../../pages/UserPage'));
-const NoticesPage = lazy(() => import('../../pages/NoticesPage'));
-const AddPetPage = lazy(() => import('../../pages/AddPetPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const MainPage = lazy(() => import('pages/MainPage'));
+const UserPage = lazy(() => import('pages/UserPage'));
+const NoticesPage = lazy(() => import('pages/NoticesPage'));
+const AddPetPage = lazy(() => import('pages/AddPetPage'));
 const OurFriendsPage = lazy(() => import('pages/OurFriendsPage'));
-const ErrorPage = lazy(() => import('../../pages/ErrorPage'));
+const ErrorPage = lazy(() => import('pages/ErrorPage'));
 
 const App = () => {
   const dispatch = useDispatch();
--- src/components/UserData/UserData.styled.jsx
+++ src/components/UserData/UserData.styled.jsx
@@ -1,11 +1,9 @@
 import styled from '@emotion/styled';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
-import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
-import CheckIcon from '@mui/icons-material/Check';
-import { Field } from 'formik';
-
-// import { styled } from '@mui/material/styles';
+import { CiEdit, CiLogout } from 'react-icons/ci';
+import { BsCamera } from 'react-icons/bs';
+import { RxCross1 } from 'react-icons/rx';
+import { AiOutlineCheck } from 'react-icons/ai';
+import { Field, Form } from 'formik';
 import { Typography } from '@mui/material';
 
 export const Box = styled.div`
@@ -34,66 +32,91 @@ export const Title = styled(Typography)(({ theme }) => ({
 export const Photo = styled.div`
   display: flex;
   justify-content: center;
+
   margin: 20px 0 15px 0;
 `;
 
 export const Img = styled.img`
   display: block;
-  max-width: 182px;
-  height: auto;
+  width: 182px;
+  height: 182px;
   border-radius: 40px;
-  background-color: #ccc;
+  background-color: #ebebeb;
 `;
 
-export const EditPhoto = styled.button`
+export const DivEditPhoto = styled.div`
+  display: flex;
+  justify-content: center;
+
+  margin-bottom: 25px;
+`;
+
+export const EditButtonPhoto = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
 
+  width: 120px;
+  height: 25px;
+
   border: none;
   background-color: #fff;
   padding: 0;
-  margin-bottom: 25px;
 
   cursor: pointer;
-  font-style: normal;
-  font-weight: 400;
   font-size: 12px;
-  line-height: 22px;
   letter-spacing: 0.04em;
 `;
-// export const MyForm = styled(Form)`
-//   display: flex;
-//   flex-direction: column;
-//   margin-bottom: 115px;
-// `;
+
+export const IconWrapperCheck = styled.div`
+  margin-right: 5px;
+`;
+
+export const IconWrapperCross = styled.div`
+  margin-left: 5px;
+`;
+
+export const MyForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Label = styled.label`
   position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 7px;
-  font-style: normal;
+  margin-bottom: 10px;
+
   font-weight: 600;
   font-size: 18px;
-  line-height: 25px;
   letter-spacing: 0.04em;
 `;
 
 export const Input = styled(Field)`
   width: 255px;
   height: 32px;
-  padding: 0 15px 0 15px;
+  padding: 0 40px 0 15px;
   border: 1px solid #54adff;
   border-radius: 20px;
   outline: none;
+  background-color: #fff;
 
-  font-style: normal;
-  font-weight: 400;
   font-size: 16px;
-  line-height: 22px;
   letter-spacing: 0.04em;
+
+  &.editing {
+    border: 1px solid #20e820;
+  }
+
+  ::placeholder {
+    color: #000000;
+  }
+`;
+
+export const DivLogOut = styled.div`
+  display: flex;
+  margin-top: 10px;
 `;
 
 export const ButtonLogOut = styled.button`
@@ -103,39 +126,54 @@ export const ButtonLogOut = styled.button`
   border: none;
   background-color: #fff;
   padding: 0;
-  margin-top: 20px;
 
   cursor: pointer;
   color: #888888;
-  font-style: normal;
-  font-weight: 500;
   font-size: 16px;
-  line-height: 22px;
   letter-spacing: 0.04em;
 `;
-export const IconCamera = styled(CameraAltOutlinedIcon)`
+
+export const IconCamera = styled(BsCamera)`
   margin-right: 10px;
   color: #54adff;
   font-size: 18px;
 `;
 
-export const IconLogOut = styled(LogoutOutlinedIcon)`
-  transform: rotate(180deg);
+export const IconLogOut = styled(CiLogout)`
   margin-right: 10px;
 
   color: #54adff;
+  font-size: 25px;
 `;
-export const IconEdit = styled(DriveFileRenameOutlineOutlinedIcon)`
+
+export const IconEdit = styled(CiEdit)`
   position: absolute;
   left: 90%;
 
+  font-size: 25px;
   cursor: pointer;
   color: #54adff;
+
+  :hover {
+    color: red;
+  }
 `;
-export const IconCheck = styled(CheckIcon)`
+
+export const DivIconCheck = styled.div`
   position: absolute;
-  left: 90%;
+  left: 90.5%;
+`;
+
+export const IconCheck = styled(AiOutlineCheck)`
+  font-size: 23px;
 
   cursor: pointer;
   color: #20e820;
+`;
+
+export const IconCross = styled(RxCross1)`
+  font-size: 18px;
+
+  cursor: pointer;
+  color: #e82d20;
 `;
