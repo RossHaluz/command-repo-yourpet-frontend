d26aaedcf47bee8f91c9b5e0af8692d9b64d288f
Vlad Makhnenko <vlmakh@gmail.com>
2023-05-29 19:55:59 +0300

    update UserData

--- src/components/UserData/UserData.styled.jsx
+++ src/components/UserData/UserData.styled.jsx
@@ -153,6 +153,10 @@ export const Input = styled(Field)`
   letter-spacing: 0.04em;
   color: #000000;
 
+  &.editing {
+    border: 1px solid #20e820;
+  }
+
   @media screen and (min-width: 768px) {
     width: 255px;
     height: 32px;
@@ -160,15 +164,15 @@ export const Input = styled(Field)`
     font-size: 16px;
     letter-spacing: 0.04em;
   }
-
-  &.editing {
-    border: 1px solid #20e820;
-  }
 `;
 
 export const DivLogOut = styled.div`
   display: flex;
-  margin-top: 15px;
+  margin-top: 25px;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 15px;
+  }
 `;
 
 export const ButtonLogOut = styled.button`
