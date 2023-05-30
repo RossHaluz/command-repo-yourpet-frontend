4cfc111f2f31470fdbe3a4976779b16cb70b9c17
Eric Ramirez <ramirez1990rer@gmail.com>
2023-05-30 12:17:04 +0300

    added PetsNotFound

---
+++ src/components/PetsData/PetsNotFound.jsx
@@ -0,0 +1,13 @@
+import { Notification, Text } from './PetsNotFound.styled';
+
+const PetsNotFound = () => {
+  return (
+    <>
+      <Notification>
+        <Text>You have not added any pets </Text>
+      </Notification>
+    </>
+  );
+};
+
+export default PetsNotFound;
